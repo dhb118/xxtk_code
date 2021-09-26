6a844c69672419ac744c3d4441a2327aa6f6b221
duanhongbo <6860300@qq.com>
2021-09-26 15:05:52 +0800

    重新提交

---
+++ README.mk
@@ -0,0 +1,8 @@
+小小坦克 公共库
+
+cocos-recast //寻路库
+
+
+recast.js 是的源码地址如下：
+https://github.com/BabylonJS/Extensions/tree/master/recastjs
+是由C++编译成js的。
--- mvc/BaseViewData.ts
+++
@@ -1,7 +0,0 @@
-/*
- * @Author: yanmingjie0223@qq.com
- * @Date: 2019-01-17 10:59:19
- * @Last Modified by: checong.brainy
- * @Last Modified time: 2020-09-27 14:49:56
- */
-export default class BaseViewData {}
